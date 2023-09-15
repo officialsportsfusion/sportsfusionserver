910cd9c31d2ef5dbeac1bb715501181b19767d15
Tony <asolomon484@gmail.com>
2023-09-15 13:11:21 +0100

    new changes

--- src/router/v1/auth/index.js
+++ src/router/v1/auth/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const upload = require('../../../utilis/upload')
 const auth = require('../../../controller/auth')
 
 router.post('/user', auth.signup)
@@ -10,6 +9,5 @@ router.post('/resend/otp', auth.resendOtp)
 router.post('/forgot/password', auth.forgotPassword)
 router.post('/change/password', auth.changePassword)
 
-// router.post('/upload/avatar/:userId', upload.single('avatar'), auth.uploadImage)
 
 module.exports = router
